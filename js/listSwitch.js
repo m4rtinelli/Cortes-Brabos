c61d5ac9380c716127147d3437a26d533c20936a
m4rtinelli <martinelli303@hotmail.com>
2025-01-07 15:44:00 -0300

    pagina sobre desktop

--- js/listSwitch.js
+++ js/listSwitch.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const listSelector = document.querySelectorAll(".list-switch span");
+  const mainImageGallery = document.querySelector(".main-gallery");
+  const listModeGallery = document.querySelector(".list-mode");
+  const liToggle = document.getElementById("li-toggle");
+  const imgToggle = document.getElementById("img-toggle");
+  const extraWorksCarrossel = document.querySelector(".extra-works-wrapper");
 
   listSelector.forEach((item) => {
     item.addEventListener("click", () => {
@@ -13,5 +18,23 @@ document.addEventListener("DOMContentLoaded", () => {
       item.classList.add("selected");
       item.innerHTML = `▪${item.innerHTML}`;
     });
+  });
+
+  // mudar o layout para aparecer as imagens ou a lista de trabalhos
+  // -------
+  liToggle.addEventListener("click", () => {
+    if (listModeGallery.classList.contains("display-none")) {
+      listModeGallery.classList.remove("display-none");
+      mainImageGallery.classList.add("display-none");
+      extraWorksCarrossel.classList.add("display-none");
+    }
+  });
+
+  imgToggle.addEventListener("click", () => {
+    if (mainImageGallery.classList.contains("display-none")) {
+      mainImageGallery.classList.remove("display-none");
+      listModeGallery.classList.add("display-none");
+      extraWorksCarrossel.classList.remove("display-none");
+    }
   });
 });
